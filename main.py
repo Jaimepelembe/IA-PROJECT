fc23ddd59b5ea6840db93eb8eb1e6ea36cce3396
lamacas8888@gmail.com <152987562+LinoMacas@users.noreply.github.com>
2024-11-12 15:25:18 +0200

    Update da identidade visual

--- main.py
+++ main.py
@@ -14,14 +14,18 @@ button_style = {
     "pad": (5, 5)
 }
 
+g = Grafo()
+g.inicializarGrafo()
+lista_nos=list(g.getGrafo().nodes)
+
 layout = [
     [sg.Text("Semáforos Inteligentes", font=("Inter", 24, "bold"), expand_x=True, justification="center", pad=(0, 10), text_color=TEXT_COLOR, background_color=BACKGROUND_COLOR)],
     [sg.Canvas(size=(800, 400), key="-CANVAS-", expand_x=True, expand_y=True, background_color="#F5EEF8", pad=(0, 10))],
     [
         sg.Text("Origem:", font=("Inter", 10), text_color=TEXT_COLOR, background_color=BACKGROUND_COLOR, pad=(5, 5)),
-        sg.Combo(["Inhagoia", "Absa", "Mogas", "Praca Magaia", "UIR", "Brigada montada", "HGJM", "Versalhes", "Jardim Majerman", "Guerra popular", "Estatua Mondlane", "Pep", "Movitel", "Ponto final", "Belita", "Tecnicol"], key="-ORIGEM-", size=(15, 1), font=("Inter", 10), pad=(5, 5), background_color="#FFFFFF"),
+        sg.Combo(lista_nos, key="-ORIGEM-", size=(15, 1), font=("Inter", 10), pad=(5, 5), background_color="#FFFFFF"),
         sg.Text("Destino:", font=("Inter", 10), text_color=TEXT_COLOR, background_color=BACKGROUND_COLOR, pad=(5, 5)),
-        sg.Combo(["Inhagoia", "Absa", "Mogas", "Praca Magaia", "UIR", "Brigada montada", "HGJM", "Versalhes", "Jardim Majerman", "Guerra popular", "Estatua Mondlane", "Pep", "Movitel", "Ponto final", "Belita", "Tecnicol"], key="-DESTINO-", size=(15, 1), font=("Inter", 10), pad=(5, 5), background_color="#FFFFFF"),
+        sg.Combo(lista_nos, key="-DESTINO-", size=(15, 1), font=("Inter", 10), pad=(5, 5), background_color="#FFFFFF"),
         sg.Text("Algoritmo:", font=("Inter", 10), text_color=TEXT_COLOR, background_color=BACKGROUND_COLOR, pad=(5, 5)),
         sg.Combo(["Profundidade Iterativa", "Busca em Custo Uniforme", "A*"], key="-ALGORITMO-", size=(20, 1), font=("Inter", 10), pad=(5, 5), background_color="#FFFFFF"),
         sg.Button("Buscar", **button_style),
@@ -35,29 +39,27 @@ window = sg.Window("Semáforos Inteligentes - Maputo", layout, resizable=True, f
 canvas_elem = window["-CANVAS-"]
 canvas = canvas_elem.TKCanvas
 
-g = Grafo()
-g.inicializarGrafo()
 
-def desenhar_grafo_no_canvas(grafo):
-    canvas.delete("all")
-    pos = g.positionOfNodes()
-    scale_factor = 20
-    offset_x, offset_y = 350, 100
+# def desenhar_grafo_no_canvas(grafo):
+#     canvas.delete("all")
+#     pos = g.positionOfNodes()
+#     scale_factor = 20
+#     offset_x, offset_y = 350, 100
 
-    for edge in grafo.edges():
-        node1, node2 = edge
-        x1, y1 = pos[node1]
-        x2, y2 = pos[node2]
-        canvas.create_line(x1 * scale_factor + offset_x, y1 * scale_factor + offset_y,
-                           x2 * scale_factor + offset_x, y2 * scale_factor + offset_y, fill="black", width=1)
+#     for edge in grafo.edges():
+#         node1, node2 = edge
+#         x1, y1 = pos[node1]
+#         x2, y2 = pos[node2]
+#         canvas.create_line(x1 * scale_factor + offset_x, y1 * scale_factor + offset_y,
+#                            x2 * scale_factor + offset_x, y2 * scale_factor + offset_y, fill="black", width=1)
 
-    for node in grafo.nodes():
-        x, y = pos[node]
-        x, y = x * scale_factor + offset_x, y * scale_factor + offset_y
-        canvas.create_oval(x-25, y-25, x+25, y+25, fill="#229954")
-        canvas.create_text(x, y, text=node, fill="white", font=("Inter", 6, "bold"))
+#     for node in grafo.nodes():
+#         x, y = pos[node]
+#         x, y = x * scale_factor + offset_x, y * scale_factor + offset_y
+#         canvas.create_oval(x-25, y-25, x+25, y+25, fill="#229954")
+#         canvas.create_text(x, y, text=node, fill="white", font=("Inter", 6, "bold"))
 
-desenhar_grafo_no_canvas(g.getGrafo())
+# desenhar_grafo_no_canvas(g.getGrafo())
 
 while True:
     event, values = window.read()
